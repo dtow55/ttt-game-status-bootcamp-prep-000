2c6a88c63322bd2b63fb691cc1f88372057523e7
David J Towster <djt4xe@virginia.edu>
2017-12-06 02:51:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,12 +21,12 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-    #if (board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X")
-    #  return win_combination
-    #elsif (board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O")
-    #  return win_combination
-    #else
-    #  return nil
-    #end
+    if (board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X")
+      return win_combination
+    elsif (board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O")
+      return win_combination
+    else
+      nil
+    end
   end
 end
